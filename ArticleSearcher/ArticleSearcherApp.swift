637e5f3a4b3e78329921acb9031bec674f346b02
Aoitoridayo <aoitoridayo1110@icloud.com>
2024-05-27 12:14:23 +0900

    Initial Commit

---
+++ ArticleSearcher/ArticleSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleSearcherApp.swift
+//  ArticleSearcher
+//
+//  Created by 渡邊魁優 on 2024/05/27.
+//
+
+import SwiftUI
+
+@main
+struct ArticleSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
